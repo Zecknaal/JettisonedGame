567eae8b02215e7209b632b2f9735974aa4928d8
Zecknaal <zecknaal@gmail.com>
2016-01-29 08:25:28 -0600

    Initial commit

---
+++ Source/JettisonedGame/JettisonedGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "JettisonedGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JETTISONEDGAME_API AJettisonedGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
